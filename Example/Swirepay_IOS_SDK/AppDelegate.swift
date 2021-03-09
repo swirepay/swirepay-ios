7e414f89209c6def50b40ecb522de470ee7b11e6
dinesh <dinesh@swirepay.com>
2021-03-09 07:37:28 +0100

    switch to debug

--- Example/Swirepay_IOS_SDK/AppDelegate.swift
+++ Example/Swirepay_IOS_SDK/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let api_key = "sk_test_7hneHju6knsZT29ZatWq5gIHNav41ls9" //
+        let api_key = "sk_test_xkNDG8VLfNYEqOMVvrMho98K60NGkuyQ" //
           //sk_test_7hneHju6knsZT29ZatWq5gIHNav41ls9 live
          // sk_test_xkNDG8VLfNYEqOMVvrMho98K60NGkuyQ debug
 
